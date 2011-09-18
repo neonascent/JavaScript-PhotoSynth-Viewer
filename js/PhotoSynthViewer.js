b9205865bc895972d3573890746ebaa9aa3f7e0d
Josh Harle <josh.harle@gmail.com>
2011-09-19 03:22:55 +1000

    Switched to minified Prototype, Cut out un-used rendering/cameras stuff removed some un-used libraries

--- js/PhotoSynthViewer.js
+++ js/PhotoSynthViewer.js
@@ -9,6 +9,10 @@
  * todo: navigation arrows subtly on image (fade? animated gif?)
  *
  * todo: remove unneccesary CoverFlow and Accordian code. 
+ *
+ * todo: Minify everything: http://jscrambler.com/compress
+ *
+ * todo: cutdown version of THREE: Quaternion, Matrix4 (Lookat), Vector3
  */
 
 
@@ -29,9 +33,6 @@ function PhotoSynthViewer(div, frame, sound) {
 	var _scene;
 	var _particleSystems = [];
 	var _lines;
-	var _pointCloudMaterial;
-	var _camerasFrustrumMaterial;
-	var _stats;
 	var _loader;	
 	
 	var _similarAngle = 10; // +- 10 degrees 
@@ -40,7 +41,6 @@ function PhotoSynthViewer(div, frame, sound) {
 	var _guid;
 	
 	//camera
-	var _camera;
 	var _qx = new THREE.Quaternion(1.0, 0.0, 0.0, 0.0);
 	var _qy = new THREE.Quaternion(0.0, 1.0, 0.0, 0.0);
 	var _qz = new THREE.Quaternion(0.0, 0.0, 1.0, 0.0);
@@ -76,11 +76,6 @@ function PhotoSynthViewer(div, frame, sound) {
 	var _currentSoundObject;
 
 	var _imgLoading = '<img src="img/loading.gif" alt="" />';
-
-	
-	this.getCamera = function() {
-		return _camera;
-	};
 	
 	this.getLoader = function() {
 		return _loader;
@@ -94,39 +89,6 @@ function PhotoSynthViewer(div, frame, sound) {
 		return _imageWidth;
 	};
 	
-	this.toggleTopView = function() {
-		if (_isCameraTopView) {
-			moveCameraTo(_prevCameraPosition, _prevCameraRotation, {
-				onComplete : function() {
-					_isCameraTopView = false;
-				}
-			});
-		}
-		else {
-			_prevCameraPosition = _camera.position.clone();
-			_prevCameraRotation = new THREE.Quaternion(); _prevCameraRotation.copy(_camera.quaternion); //TODO: Three.js I need clone() on Quaternion too!
-			moveCameraTo(new THREE.Vector3(0, 0, 10), new THREE.Quaternion(0, 0, 0 ,1), {
-				onComplete : function() {
-					_isCameraTopView = true;
-				}
-			});		
-		}
-	};
-	
-	this.setPointSize = function(pointSize) {
-		_pointCloudMaterial.size = pointSize;
-	};
-	
-	this.setCamerasFrustrumVisibility = function(isVisible) {
-		_camerasFrustrumMaterial.opacity = (isVisible ? 1.0 : 0.0);
-	};
-	
-	/*this.setCamera = function(coordSystemIndex, cameraIndex) {
-		var cam = _loader.getCamera(coordSystemIndex, cameraIndex);
-		_camera.position = new THREE.Vector3(cam.position[0], cam.position[1], cam.position[2]);
-		_camera.quaternion = new THREE.Quaternion(cam.orientation[0], cam.orientation[1], cam.orientation[2], cam.orientation[3]);
-		_camera.quaternion.multiplySelf(_qx);
-	};*/
 
 	this.moveToCamera = function (coordSystemIndex, cameraIndex, options) {
 	    // record the current camera index
@@ -213,16 +175,6 @@ function PhotoSynthViewer(div, frame, sound) {
 		  });
 		}
 
-
-	// move in the angle direction
-	/*this.moveDirection = function(angle) {
-		var newCamera = _loader.basicCardinalMovement(_currentCoordIndex, _currentCameraIndex, angle);
-		if (newCamera) {
-			this.MoveToCamera(_currentCoordIndex, newCamera, {}); 
-			alert(newCamera);
-		}
-	}*/
-	
 	
 	// can "cache" many of these things.  Nearest cameras, possible angles, etc. 
 	
@@ -515,39 +467,8 @@ function PhotoSynthViewer(div, frame, sound) {
 	       return rotation;
 	   }
 	
-	function moveCameraTo(dstPosition, dstRotation, options) {		
-		var _onUpdate   = options.onUpdate     || function() {};
-		var _onComplete = options.onComplete || function() {};
-		
-		var srcPosition = new THREE.Vector3(_camera.position.x, _camera.position.y, _camera.position.z);
-		var srcRotation = new THREE.Quaternion(_camera.quaternion.x, _camera.quaternion.y, _camera.quaternion.z, _camera.quaternion.w);
-		
-		new Fx(_camera, {
-			transition: Fx.Transitions.Quint.easeOut,
-			onApply: function(elt, percent) {
-				var src = new THREE.Vector3(srcPosition.x, srcPosition.y, srcPosition.z);
-				var dst = new THREE.Vector3(dstPosition.x, dstPosition.y, dstPosition.z);
-				_camera.position = src.multiplyScalar(1-percent).addSelf(dst.multiplyScalar(percent)); //src*(1-p) + dst*p
-				THREE.Quaternion.slerp(srcRotation, dstRotation, _camera.quaternion, percent);
-				_onUpdate();
-			},
-			onComplete : function() {
-				_camera.position   = dstPosition;
-				_camera.quaternion = dstRotation;
-				_onComplete();
-			}
-		});	
-	}
-	
 
-	function setupScene() {
-		
-		_camera   = new THREE.Camera(45, _width / _height, 0.01, 1000); //fov ratio near far
-		_camera.position.z = 20;
-		_camera.useTarget = false;
-		_camera.useQuaternion = true;
-		
-		
+	function setupScene() {		
 		
 		if (_useSound) {
 			// disable debug mode after development/testing..
@@ -566,61 +487,8 @@ function PhotoSynthViewer(div, frame, sound) {
 				alert("Sound failed to start!");
 
 			});
-		}
-		
-		
-		//_renderer = new THREE.WebGLRenderer();		
-		//_scene    = new THREE.Scene();
-		//_renderer.setSize(_width, _height);
-		//_container.appendChild(_renderer.domElement);
-		
-
-		/*
-		Event.observe(_renderer.domElement, "mousemove", function(event) {
-			//console.log("mouse move");
-		});
-
-		Event.observe(_renderer.domElement, "mousedown", function(event) {
-			//console.log("mouse down");
-		});
-
-		Event.observe(_renderer.domElement, "mouseup", function(event) {
-			//console.log("mouse up");
-		});
-		
-		Event.observe(_renderer.domElement, "keydown", function(event) {
-			alert("keypress");
-			console.log("keydown");
-		});
-
-		Event.observe(_renderer.domElement, "keyup", function(event) {
-			console.log("keyup");
-		});*/
-		
-	
-		_stats = new Stats();
-		_stats.domElement.style.display = "inline-block";
-		//_controller.getElementsByTagName("p")[0].appendChild(_stats.domElement);
-		
+		}	
 			
-		_pointCloudMaterial = new THREE.ParticleBasicMaterial({
-			size: 0.0125,
-			vertexColors : true
-		});
-		
-		_camerasFrustrumMaterial = new THREE.LineBasicMaterial({ 
-			color: 0xffffff, 
-			opacity: 1, 
-			linewidth: 3, 
-			vertexColors: false
-		});
-
-		/*
-		setInterval(function(){
-			_renderer.render(_scene, _camera);
-			_stats.update();
-		}, 1000/60);*/
-		
 	}
 	
 	setupScene();
@@ -646,72 +514,11 @@ function PhotoSynthViewer(div, frame, sound) {
 	}
 	
 	function initCoordSystem(metadataLoader, coordSystemIndex) {
-		clearScene();
-		_particleSystems = new Array(metadataLoader.getNbBinFiles(coordSystemIndex));	
-		drawCamerasFrustrum(metadataLoader, coordSystemIndex);
+		_particleSystems = new Array(metadataLoader.getNbBinFiles(coordSystemIndex));
+		_thumbs = metadataLoader.getJsonInfo();
 	}
 	
-	function drawCamerasFrustrum(metadataLoader, coordSystemIndex) {
-	
-		var nbCameras = metadataLoader.getNbCameras(coordSystemIndex);
-		var coordSystem = metadataLoader.getCoordSystem(coordSystemIndex);
-		
-		var distanceFromCamera = 0.2;
-		var geometry = new THREE.Geometry();
-		
-		for (var i=0; i<coordSystem.cameras.length; ++i) {
-			if (coordSystem.cameras[i] !== undefined) {
-				var cam = coordSystem.cameras[i];
-				
-				
-				_thumbs = metadataLoader.getJsonInfo();
-				
-				var thumb  = _thumbs.thumbs[i];
-														
-				var width  = thumb.width;
-				var height = thumb.height;
-				var focalLength = cam.focal*Math.max(width, height);
-				var aspectRatio = width / height;
-				var fovy = 2.0 * Math.atan(height / (2.0*focalLength));
-				var planeHeight = 2 * distanceFromCamera * Math.tan(fovy*0.5);
-				var planeWidth  = planeHeight * aspectRatio;
-				
-				var pos = new THREE.Vector3(cam.position[0], cam.position[1], cam.position[2]);
-				var rot = new THREE.Quaternion(cam.orientation[0], cam.orientation[1], cam.orientation[2], cam.orientation[3]);
-				rot.multiplySelf(_qx);
-								
-				var c = new THREE.Vector3(); c.add(pos, rot.multiplyVector3(new THREE.Vector3(-planeWidth/2,  planeHeight/2, -distanceFromCamera), new THREE.Vector3()));
-				var d = new THREE.Vector3(); d.add(pos, rot.multiplyVector3(new THREE.Vector3( planeWidth/2,  planeHeight/2, -distanceFromCamera), new THREE.Vector3()));
-				var e = new THREE.Vector3(); e.add(pos, rot.multiplyVector3(new THREE.Vector3( planeWidth/2, -planeHeight/2, -distanceFromCamera), new THREE.Vector3()));
-				var f = new THREE.Vector3(); f.add(pos, rot.multiplyVector3(new THREE.Vector3(-planeWidth/2, -planeHeight/2, -distanceFromCamera), new THREE.Vector3()));
-				
-				geometry.vertices.push(new THREE.Vertex(c));
-				geometry.vertices.push(new THREE.Vertex(d));
-				geometry.vertices.push(new THREE.Vertex(d));
-				geometry.vertices.push(new THREE.Vertex(e));
-				geometry.vertices.push(new THREE.Vertex(e));
-				geometry.vertices.push(new THREE.Vertex(f));
-				geometry.vertices.push(new THREE.Vertex(f));
-				geometry.vertices.push(new THREE.Vertex(c));
-				
-				geometry.vertices.push(new THREE.Vertex(pos));
-				geometry.vertices.push(new THREE.Vertex(c));
-				geometry.vertices.push(new THREE.Vertex(pos));
-				geometry.vertices.push(new THREE.Vertex(d));
-				geometry.vertices.push(new THREE.Vertex(pos));
-				geometry.vertices.push(new THREE.Vertex(e));
-				geometry.vertices.push(new THREE.Vertex(pos));
-				geometry.vertices.push(new THREE.Vertex(f));				
-			}
-		}
 
-		
-		
-		_lines =  new THREE.Line(geometry, _camerasFrustrumMaterial, THREE.LinePieces);
-		_camerasFrustrumMaterial.opacity = 0;
-		//_scene.addObject(_lines);
-	}
-	
 	// josh: works out Euler angles (ported from: http://forums.create.msdn.com/forums/p/4574/232603.aspx#232603 )
 	function QuaternionToEuler(q) 
         { 
@@ -749,14 +556,6 @@ function PhotoSynthViewer(div, frame, sound) {
             return v; 
         }  
 
-	//to fix odd mixup of x and y
-	/*function QuaternionToEuler(q) {
-		var temp = brokenQuaternionToEuler(q);
-		
-		return new THREE.Vector3(temp.y, temp.x, temp.z);
-		
-	}*/
-	
 	//josh: vector to Degrees
 	function vToDegrees(v) {
 		v.x = v.x * (180/Math.PI);
@@ -777,51 +576,15 @@ function PhotoSynthViewer(div, frame, sound) {
 		return relative;
 	}
 	
-	/*
-	function updateCoordSystem(metadataLoader, coordSystemIndex, binFileIndex) {
-		var coordSystem = metadataLoader.getCoordSystem(coordSystemIndex);
-		coordSystem.pointClouds[binFileIndex] = new THREE.Geometry();
-		
-		var pointCloud = coordSystem.pointClouds[binFileIndex];
-		for (var i=0; i<coordSystem.binFiles[binFileIndex].positions.length/3; ++i) {
-			var vertex = new THREE.Vertex(new THREE.Vector3(coordSystem.binFiles[binFileIndex].positions[i*3+0], 
-															coordSystem.binFiles[binFileIndex].positions[i*3+1], 
-															coordSystem.binFiles[binFileIndex].positions[i*3+2]));
-			pointCloud.vertices.push(vertex);
-			var color = new THREE.Color(0xffffff);
-			color.setRGB(coordSystem.binFiles[binFileIndex].colors[i*3+0], 
-						 coordSystem.binFiles[binFileIndex].colors[i*3+1], 
-						 coordSystem.binFiles[binFileIndex].colors[i*3+2]);
-			pointCloud.colors.push(color);
-		}		
-		_particleSystems[binFileIndex] = new THREE.ParticleSystem(pointCloud, _pointCloudMaterial);
-		_scene.addChild(_particleSystems[binFileIndex]);
-	}*/
-	
-	function clearScene() {
-		/*for (var i=0; i<_particleSystems.length; ++i)
-			_scene.removeChild(_particleSystems[i]);
-		_particleSystems = [];
-		
-		if (_lines) {
-			_scene.removeChild(_lines);
-		}
-		_lines = undefined;*/
-	}
+
+
 	
 	this.load = function(guid) {
 		
 		_guid = guid;
-		
-		var loaderInfo = _div.getElementsByClassName("loader-info")[0];
-		loaderInfo.innerHTML = _imgLoading;
-		
-		var infoPanel = _div.getElementsByClassName("info-panel")[0];
-		infoPanel.innerHTML = "";
-		
+			
 		_loader = new PhotoSynthMetadataLoader(guid, {
 			onComplete : function() {
-				loaderInfo.innerHTML = "Loaded";
 				// just load the first one!
 				index = 0;
 				initCoordSystem(_loader, index);
@@ -835,169 +598,6 @@ function PhotoSynthViewer(div, frame, sound) {
 						}							
 					}
 				});
-			},
-			onProgress : function(loader) {
-				if (loader.state == 0) {
-					loaderInfo.innerHTML = "Loading Soap " + _imgLoading;
-				}
-				else if (loader.state == 1) {
-					loaderInfo.innerHTML = "Loading Json " + _imgLoading;
-				}
-				else if (loader.state == 2)
-				{
-					var ul = new Element("ul", {"class": "acc"});
-					
-					for (var i=0; i<loader.getNbCoordSystems(); ++i) {
-						
-						var nbCameras        = loader.getNbCameras(i);
-						var approxNbVertices = loader.getNbBinFiles(i) * 5000;
-						
-						if (nbCameras > 2) {
-							var li = new Element("li");
-							var h3 = new Element("h3");
-							var span = new Element("span", {"class": "right"});
-							
-							span.appendChild(document.createTextNode(nbCameras + " images ~" + approxNbVertices/1000+"k vertices"));
-							h3.appendChild(document.createTextNode("Coord system " + i + ":"));
-							h3.appendChild(span);							
-							li.appendChild(h3);							
-							
-							var divAccSection = new Element("div", {"class" : "acc-section" });
-							var divAccContent = new Element("div", {"class" : "acc-content"});
-							var approxSize = loader.getNbBinFiles(i) * 83;
-							approxSize = (approxSize < 1024) ? approxSize + "ko" : Math.round(approxSize/10)/100+"Mo";
-							var input = new Element("input", { type: "button", 'class': 'load', value : "Load (~"+approxSize+")" });
-							
-							divAccContent.appendChild(input);
-							divAccSection.appendChild(divAccContent);
-							li.appendChild(divAccSection);
-							
-							(function(div_elt) {
-								Event.observe(input, "click", function(index) {
-									return function() {
-										initCoordSystem(loader, index);
-										loader.loadCoordSystem(index, {
-											onStart : function() {											
-												var firstCameraIndex = getIndexOfFirstCamera(loader, index);
-												_currentCoordIndex = index;
-												if (firstCameraIndex != -1) {
-													//_that.setCamera(index, firstCameraIndex);
-													_currentCameraIndex = firstCameraIndex;
-													_that.moveToCamera(_currentCoordIndex, _currentCameraIndex, {});
-												}
-																								
-											},
-											onProgress : function(loader, coordSystemIndex, binFileIndex, percent) {
-												div_elt.innerHTML = Math.round(percent*100)+"%";
-												//updateCoordSystem(loader, coordSystemIndex, binFileIndex);
-											},											
-											onComplete : function() {
-												div_elt.innerHTML = "";
-												
-												var span = div_elt.parentNode.parentNode.getElementsByTagName("h3")[0].getElementsByTagName("span")[0];
-												span.innerHTML = "";
-												var nbCameras  = loader.getNbCameras(index);
-												var nbVertices = loader.getNbVertices(index);
-												if (nbVertices > 1000) 
-													nbVertices = Math.round(nbVertices/1000)+"k";												
-												span.appendChild(document.createTextNode(" (" + nbCameras + " images, " + nbVertices + " vertices) "));
-												
-												/*var p = new Element("p", {'class': 'download'});
-												p.appendChild(document.createTextNode("Download ply: "));
-												var span0 = document.createElement("span");
-												p.appendChild(span0);
-												p.appendChild(document.createTextNode(", with cameras: "));
-												var span1 = document.createElement("span");
-												p.appendChild(span1);
-												
-												div_elt.appendChild(p);
-												
-												Downloadify.create(span0, {
-													filename: 'coord_system_' + index + '.ply',
-													data: function() { return _that.getCoordSystemAsPly(loader, index, false); },
-													swf: 'swf/downloadify.swf',
-													downloadImage: 'img/downloadify.png',
-													width: 16,
-													height: 16,
-													transparent: true,
-													append: true
-												});
-												
-												Downloadify.create(span1, {
-													filename: 'coord_system_' + index + '_with_cameras.ply',
-													data: function() { return _that.getCoordSystemAsPly(loader, index, true); },
-													swf: 'swf/downloadify.swf',
-													downloadImage: 'img/downloadify.png',
-													width: 16,
-													height: 16,
-													transparent: true,
-													append: true
-												});*/
-												
-												var coordSystem = loader.getCoordSystem(index);												
-												
-												/*
-												var select = new Element("select", {});
-												Event.observe(select, "change", function() {
-													_that.moveToCamera(index, this.value, {});
-												});
-												
-												var cameraIndex = 0;
-												for (var i=0; i<coordSystem.cameras.length; ++i) {
-													if (coordSystem.cameras[i] !== undefined) {
-														var option = new Element("option", { value: i});
-														option.appendChild(document.createTextNode("camera "+cameraIndex));
-														select.appendChild(option);														
-														cameraIndex++;
-													}
-												}				
-												div_elt.appendChild(select);												
-												*/
-												
-												var divCoverflow = new Element("div", {'class': 'imageflow', 'id': 'coverflow_'+index});
-												for (var i=0; i<coordSystem.cameras.length; ++i) {
-													if (coordSystem.cameras[i] !== undefined) {
-														var img = new Element("img", {alt: "", src: _loader.getJsonInfo().thumbs[i].url, index: i});
-														divCoverflow.appendChild(img);
-													}
-												}
-												div_elt.appendChild(divCoverflow);
-												var coverflow = new ImageFlow();
-												coverflow.init({
-													ImageFlowID:'coverflow_'+index , 
-													reflections: false, 
-													onClick: function() {
-														_that.moveToCamera(index, this.getAttribute('index'), {
-															onComplete: function() {
-																_isCameraTopView = false;
-															}
-														});
-													}
-												});
-											}
-										});
-									}
-								}(i));
-							})(divAccContent)
-							
-							ul.appendChild(li);
-						}
-					}
-					infoPanel.appendChild(ul);
-					new Accordion(ul, {
-						onChange : function(index) {
-							if (_loader.isCoordSystemLoaded(index)) {
-								displayCoordSystem(loader, index);																
-								var firstCameraIndex = getIndexOfFirstCamera(loader, index);
-								if (firstCameraIndex != -1) {
-									_that.setCamera(index, firstCameraIndex);
-									_currentCameraIndex = firstCameraIndex;
-								}
-																		
-							}
-						}
-					});
-				}
 			}
 		});
 	};
